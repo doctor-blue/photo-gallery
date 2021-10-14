72aacfed69d61cb6508f60263c3a12f5aa272684
Nguyen Van Tan <doctorblue.king@gmail.com>
2021-10-14 16:24:19 +0700

    Add glide

--- app/src/main/java/com/devcomentry/photogallery/presention/albums/AlbumsScreen.kt
+++ app/src/main/java/com/devcomentry/photogallery/presention/albums/AlbumsScreen.kt
@@ -7,7 +7,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.navigation.NavController
+import com.skydoves.landscapist.CircularReveal
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun AlbumsScreen(
@@ -18,9 +21,18 @@ fun AlbumsScreen(
         modifier = Modifier.fillMaxSize()
     ) {
         // Change the logo
-        Text(
-            "Hí anh em!!! This is our albums screen",
-            style = MaterialTheme.typography.h5
+//        Text(
+//            "Hí anh em!!! This is our albums screen",
+//            style = MaterialTheme.typography.h5
+//        )
+        GlideImage(
+            imageModel = "https://lh3.googleusercontent.com/W9Wg7CEvYdaVR5s6Z70aSFu8IAukRQrm9S6uEdhPw-YqTxoB-qYv-fFEsjPBW4VEXYtBIO-XOTHkkmy5wkXQdGZEQQ=w640-h400-e365-rj-sc0x00ffffff",
+            // Crop, Fit, Inside, FillHeight, FillWidth, None
+            contentScale = ContentScale.Crop,
+            // shows an image with a circular revealed animation.
+            circularReveal = CircularReveal(duration = 250),
+            // shows a placeholder ImageBitmap when loading.
+            // shows an error ImageBitmap when the request failed.
         )
     }
 }
--- app/src/main/java/com/devcomentry/photogallery/presention/all_file/AllFileScreen.kt
+++ app/src/main/java/com/devcomentry/photogallery/presention/all_file/AllFileScreen.kt
@@ -7,7 +7,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.navigation.NavController
+import com.skydoves.landscapist.CircularReveal
+import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun AllFileScreen(
@@ -21,6 +25,15 @@ fun AllFileScreen(
         Text(
             "Hí anh em!!! This is our photos screen",
             style = MaterialTheme.typography.h5
+        )
+        GlideImage(
+            imageModel = "https://lh3.googleusercontent.com/W9Wg7CEvYdaVR5s6Z70aSFu8IAukRQrm9S6uEdhPw-YqTxoB-qYv-fFEsjPBW4VEXYtBIO-XOTHkkmy5wkXQdGZEQQ=w640-h400-e365-rj-sc0x00ffffff",
+            // Crop, Fit, Inside, FillHeight, FillWidth, None
+            contentScale = ContentScale.Crop,
+            // shows an image with a circular revealed animation.
+            circularReveal = CircularReveal(duration = 250),
+            // shows a placeholder ImageBitmap when loading.
+            // shows an error ImageBitmap when the request failed.
         )
     }
 }
