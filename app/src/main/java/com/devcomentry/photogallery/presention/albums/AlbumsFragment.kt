f12cb9eb70cba38233d8557cafe6ebd071451797
minhvuhoang <hn.minhvu@gmail.com>
2021-10-24 12:00:14 +0700

    Fix show albums

--- app/src/main/java/com/devcomentry/photogallery/presention/albums/AlbumsFragment.kt
+++ app/src/main/java/com/devcomentry/photogallery/presention/albums/AlbumsFragment.kt
@@ -1,34 +1,36 @@
 package com.devcomentry.photogallery.presention.albums
 
 import android.os.Bundle
-import androidx.fragment.app.viewModels
+import android.util.Log
 import androidx.recyclerview.widget.GridLayoutManager
 import com.devcomentry.photogallery.R
 import com.devcomentry.photogallery.databinding.FragmentAlbumsBinding
 import com.devcomentry.photogallery.presention.common.BaseFragment
-import com.devcomentry.photogallery.presention.common.LocalDataViewModel
+import com.devcomentry.photogallery.presention.utils.gone
+import com.devcomentry.photogallery.presention.utils.show
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AlbumsFragment : BaseFragment<FragmentAlbumsBinding>(R.layout.fragment_albums) {
 
-    private val viewModel: LocalDataViewModel by viewModels()
     private val albumAdapter: AlbumAdapter by lazy { AlbumAdapter(requireContext(), onAlbumClick) }
 
     override fun initControls(savedInstanceState: Bundle?) {
         super.initControls(savedInstanceState)
 
-        viewModel.getData()
-
-        viewModel.dataLocal.observe(viewLifecycleOwner) { dataLocal ->
+        localDataViewModel.dataLocal.observe(viewLifecycleOwner) { dataLocal ->
             val folderDetails = mutableListOf<FolderDetail>()
             dataLocal.folder.forEach { folder ->
                 folderDetails.add(
                     FolderDetail(
                         folder,
-                        dataLocal.file.filter { it.idFolder == folder.id }))
+                        dataLocal.file.filter { it.idFolder == folder.id })
+                )
             }
+
             albumAdapter.submitList(folderDetails)
+            showEmptyLisLayout(folderDetails.isEmpty())
+            Log.d("Album", "initControls: ${dataLocal.folder.size}")
         }
 
         binding.rcvListAlbum.apply {
@@ -45,6 +47,21 @@ class AlbumsFragment : BaseFragment<FragmentAlbumsBinding>(R.layout.fragment_alb
 
     private val onAlbumClick: (FolderDetail) -> Unit = {
 //        navController.navigate()
+    }
+
+    private fun showEmptyLisLayout(isVisible: Boolean) {
+        if (isVisible) {
+            binding.apply {
+                llListEmpty.show()
+                rcvListAlbum.gone()
+            }
+        } else {
+            binding {
+                llListEmpty.gone()
+                rcvListAlbum.show()
+            }
+
+        }
     }
 
 }
